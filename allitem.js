36ff44513a57f4dffc1cd0cdb7f4ed1ef2398d2b
rawmastr <31912222+rawmastr@users.noreply.github.com>
2023-03-23 06:22:22 +0300

    Update allitem.js

--- allitem.js
+++ allitem.js
@@ -6,7 +6,7 @@ $(document).ready(function(){function relatedPost(g,e,r){$.ajax({url:"/feeds/pos
 var p=t.feed.entry[x].title.$t;var c=t.feed.entry[x].content.$t;var y=$('<div>').html(c);if(c.indexOf("https://www.youtube.com/embed/")>-1||c.indexOf("https://www.youtube.com/embed/")>-1){var d=t.feed.entry[x].media$thumbnail.url,m=d.replace('/default.jpg','/mqdefault.jpg'),k=m;}else if(c.indexOf("<img")>-1){var s=y.find('img:first').attr('src'),v=s.replace('s72-c','s100');var k=v;}else{var k='https://2.bp.blogspot.com/-4lZ7DCckjkg/WtaPclghMGI/AAAAAAAAN00/4Cais5iSDRwwUyU6jEc7qlCojlg1izsVgCLcBGAs/s1600/noImage.png';}
 h+='<li><div class="related-thumb"><a class="related-img lazyload" href="'+u+'" style="background:url('+k+') no-repeat center center;background-size: cover"/></div><h3 class="related-title"><a href="'+u+'">'+p+'</a></h3></li>'}
 h+='</div>',g.html(h);}})}
-$("#related-posts").each(function(){var g=$(this),e=g.text(),r=9;relatedPost(g,e,r);});});
+$("#related-posts").each(function(){var g=$(this),e=g.text(),r=6;relatedPost(g,e,r);});});
 // Video Responsive
 setTimeout(function(){$(".video-youtube").each(function(){$(this).replaceWith('<iframe class="video-youtube loader" src="'+$(this).data("src")+'" allowfullscreen="allowfullscreen" height="281" width="500"></iframe>')})},5e3);
 // Post image banner
